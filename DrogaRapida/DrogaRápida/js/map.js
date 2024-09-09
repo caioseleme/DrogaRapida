795cf899042ca8aedc6645802e369292aa07ff5c
Caio Seleme Azevedo <118737844+caioseleme@users.noreply.github.com>
2024-09-09 15:19:03 -0300

    Add files via upload

--- DrogaRapida/DrogaRápida/js/map.js
+++ DrogaRapida/DrogaRápida/js/map.js
@@ -1,26 +1,41 @@
-function initMap() {
-    // Localização inicial do mapa
-    const center = { lat: -23.5505, lng: -46.6333 }; // Coordenadas de São Paulo, SP (substitua por sua localização desejada)
-
-    // Criação do mapa
-    const map = new google.maps.Map(document.getElementById('map'), {
-        center: center,
-        zoom: 14
+async function init() {
+    await customElements.whenDefined('gmp-map');
+  
+    const map = document.querySelector('gmp-map');
+    const marker = document.querySelector('gmp-advanced-marker');
+    const placePicker = document.querySelector('gmpx-place-picker');
+    const infowindow = new google.maps.InfoWindow();
+  
+    map.innerMap.setOptions({
+      mapTypeControl: false
     });
-
-    // Dados das farmácias (exemplo)
-    const pharmacies = [
-        { lat: -23.5520, lng: -46.6340, name: 'Farmácia A' },
-        { lat: -23.5490, lng: -46.6350, name: 'Farmácia B' },
-        { lat: -23.5510, lng: -46.6310, name: 'Farmácia C' }
-    ];
-
-    // Adiciona um marcador para cada farmácia
-    pharmacies.forEach(pharmacy => {
-        new google.maps.Marker({
-            position: { lat: pharmacy.lat, lng: pharmacy.lng },
-            map: map,
-            title: pharmacy.name
-        });
+  
+    placePicker.addEventListener('gmpx-placechange', () => {
+      const place = placePicker.value;
+  
+      if (!place.location) {
+        window.alert(
+          "No details available for input: '" + place.name + "'"
+        );
+        infowindow.close();
+        marker.position = null;
+        return;
+      }
+  
+      if (place.viewport) {
+        map.innerMap.fitBounds(place.viewport);
+      } else {
+        map.center = place.location;
+        map.zoom = 17;
+      }
+  
+      marker.position = place.location;
+      infowindow.setContent(
+        `<strong>${place.displayName}</strong><br>
+         <span>${place.formattedAddress}</span>
+      `);
+      infowindow.open(map.innerMap, marker);
     });
-}
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
